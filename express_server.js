25586386705b299aec161e10ce2ccca439febdb7
DOwdev <63557295+DOwdev@users.noreply.github.com>
2021-03-30 22:44:04 -0400

    added delete button

--- express_server.js
+++ express_server.js
@@ -55,6 +55,12 @@ app.get("/urls/:shortURL", (req, res) => {
     res.redirect(longURL);
  }) 
 
+ app.post('/urls/:shortURL/delete', (req,res) => {
+    let shortURL = req.params.shortURL;
+    delete urlDatabase[shortURL];
+    res.redirect("/urls");
+ })
+
 app.get('/urls.json', (req,res)=>{
     res.json(urlDatabase);
 });

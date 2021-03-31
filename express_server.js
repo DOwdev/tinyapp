051fcaff12bd67ade8e7599b6b67e7125b898462
DOwdev <63557295+DOwdev@users.noreply.github.com>
2021-03-30 22:13:04 -0400

    added crypto generation of random unique id

--- express_server.js
+++ express_server.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const crypto = require('crypto');
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+
+//helper function ->  will move later
+function generateRandomString(){
+    let id = crypto.randomBytes(64).toString('hex');
+    id = id.substring(0, 6);
+    console.log(id);
+    return id;
+}
+generateRandomString();
 
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
@@ -20,6 +33,11 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/new", (req,res)=>{
     res.render("urls_new");
+});
+
+app.post("/urls", (req,res)=>{
+    console.log(req.body);
+    res.send('ok');
 });
 
 app.get("/urls/:shortURL", (req, res) => {
